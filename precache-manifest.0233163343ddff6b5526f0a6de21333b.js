84121c8a5f023b2ec81dc2eea834af19e912dc2d
Bill <207559339@qq.com>
2020-03-02 18:23:18 +0800

    Updates

---
+++ precache-manifest.0233163343ddff6b5526f0a6de21333b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a41608f9a5b3fc4db9a20271b8c3cdc",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "25f80a726c8cc835a77e",
+    "url": "/WeatherApp/static/js/2.dc44c4b9.chunk.js"
+  },
+  {
+    "revision": "f1388e66fd3a5a758577",
+    "url": "/WeatherApp/static/js/main.d0a10abd.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  }
+]);
